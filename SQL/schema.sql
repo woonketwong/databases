e1469bbc1d4a2aec830614463437e2e87204e94e
Hack Reactor Students <github@hackreactor.com>
2013-10-18 16:18:20 -0700

    Adds first draft of schema.sql

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,13 +1,48 @@
+DROP DATABASE IF EXISTS `chat`;
+
 CREATE DATABASE chat;
 
 USE chat;
-
-CREATE TABLE messages (
- /* Describe your table here.*/
-);
 
 /* You can also create more tables, if you need them... */
 
 /*  Execute this file from the command line by typing:
  *    mysql < schema.sql
  *  to create the database and the tables.*/
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` TINYINT AUTO_INCREMENT,
+  `user_id` TINYINT(50),
+  `room_id` TINYINT(100),
+  `text` VARCHAR(140),
+  `createdAt` TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` TINYINT AUTO_INCREMENT,
+  `username` VARCHAR(50),
+  `room_id` TINYINT,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `rooms`;
+
+CREATE TABLE `rooms` (
+  `id` TINYINT AUTO_INCREMENT,
+  `roomName` TINYINT,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `users_friends`;
+
+CREATE TABLE `users_friends` (
+  `id` TINYINT AUTO_INCREMENT,
+  `user_id` TINYINT,
+  `friend_id` TINYINT,
+  PRIMARY KEY (`id`)
+);
